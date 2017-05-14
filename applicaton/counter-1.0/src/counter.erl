d36cae01a398aa359b1d741f78fcd10c1d31ae6f
Stu Heiss <sheiss@highlandsolutions.com>
2017-05-14 14:43:51 -0500

    Simplify application

--- applicaton/counter-1.0/src/counter.erl
+++ applicaton/counter-1.0/src/counter.erl
@@ -1,50 +1,38 @@
-% counter client using gen_server behaviour
+%% counter client using gen_server behaviour
+%%
+%% call counter_sup:start_link/1 to start a supervisor and counter
+%% call counter_sup:stop/0 to stop supervisor and its counter
+%%
 -module(counter).
 -behavior(gen_server).
 
--export([read/0,tick/1,stop/0]).
+-export([read/0,tick/1]).
 -export([init/1,handle_call/3,handle_cast/2]).
 % add exports if you override defaults
 -export([handle_info/2, terminate/2, code_change/3]).
 
-%% for non-supervised version
-%-export([start_link/0, start_link/1]).
-
-% for supervisor version
--export([start/0,start/1]).
+% supervisor callback
+-export([start/1]).
 
 %% API
-%% non supervised version
-%% Args is the initial state
-%start_link(Args) ->
-%  io:format("~s:start_link(~p)~n",[?MODULE,Args]),
-%  gen_server:start_link({global, ?MODULE}, ?MODULE, Args, []).
-%start_link() ->
-%  start_link(0).
-
-% supervisor version
-% This will be called by the supervisor's start_link/1
-% Args is the initial state
-start(Args) ->
-  io:format("~s:start(~p)~n",[?MODULE,Args]),
-  gen_server:start_link({global, ?MODULE}, ?MODULE, Args, []).
-start() ->
-  start(0).
-
 read() ->
   gen_server:call({global,?MODULE}, read).
 
 tick(N) ->
   gen_server:cast({global,?MODULE}, {tick, N}).
 
-% if the gen_server is not part of a supervision tree, provide stop/1
-stop() ->
-  io:format("~s:stop()~n",[?MODULE]),
-  gen_server:cast({global,?MODULE}, stop).
-
 %% callbacks
+% DO NOT CALL start/1 - this is the supervisor's job!
+% called by counter_sup:init/1
+% Args is the initial state
+start(Args) ->
+  io:format("~s:start(~p)~n",[?MODULE,Args]),
+  gen_server:start_link({global, ?MODULE}, ?MODULE, Args, []).
+
+% called by gen_server:start_link from counter:start/1
 init(Args) ->
   io:format("~s:init(~p)~n",[?MODULE,Args]),
+  process_flag(trap_exit,true), % ensure call terminate/2 when 'EXIT' received
   {ok, Args}.
 
 % read
@@ -52,23 +40,18 @@ handle_call(read, _From, State) ->
   Reply=State,
   {reply, Reply, State}.
 
-% stop (only needed if the gen_server is not part of a supervision tree)
-handle_cast(stop, State) ->
-  {stop, normal, State};
 % tick
 handle_cast({tick, N}, State) ->
   NewState=State + N,
   {noreply, NewState}.
 
-% default callback implementations
-% can override with specific implementations
-
-handle_info(_Info, State) ->
-  io:format("~s:handle_info(~p,~p)~n",[?MODULE,_Info,State]),
+% override default callback implementations
+handle_info(Info, State) ->
+  io:format("~s:handle_info(~p,~p)~n",[?MODULE,Info,State]),
   {noreply, State}.
 
-terminate(_Reason, _State) ->
-  io:format("~s:terminate(~p,~p)~n",[?MODULE,_Reason,_State]),
+terminate(Reason, State) ->
+  io:format("~s:terminate(~p,~p)~n",[?MODULE,Reason,State]),
   ok.
 
 code_change(_OldVsn, State, _Extra) ->
